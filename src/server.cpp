84a72583a4974643b1d2ad99da262cfbe8008172
Zizii2 <meglado20@gmail.com>
2025-05-08 17:49:42 +0200

    init includes

--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main(int, char**){
     std::cout << "Hello, from chat_room_build!\n";
